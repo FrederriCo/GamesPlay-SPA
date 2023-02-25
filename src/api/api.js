d074813475a24f04dd890625079551727e124a84
Ferdi Mehmedov <FredericCo@mail.bg>
2023-02-25 13:18:43 +0200

    const create get, post, put, delete for request

--- src/api/api.js
+++ src/api/api.js
@@ -39,3 +39,8 @@ async function request(method, url, data) {
         throw err;
     }
 }
+
+export const get = request.bind(null, 'get');
+export const post = request.bind(null, 'post');
+export const put = request.bind(null, 'put');
+export const del = request.bind(null, 'delete');
