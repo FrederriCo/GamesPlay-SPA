7649412aa1b2d3005fc07b672d96137bf7858248
Ferdi Mehmedov <FredericCo@mail.bg>
2023-02-25 10:48:32 +0200

    create function SubmitHandler in utll.js

--- src/utl.js
+++ src/utl.js
@@ -19,3 +19,12 @@ export function setUserData() {
     localStorage.setItem('user', JSON.stringify(data));
 }
 
+export function createSubmitHandler(ctx, handler) {
+    return function (event) {
+        event.preventDefault();
+        const formData = Object.fromEntries(new FormData(event.target));
+
+        handler(ctx, formData, event);
+    };
+
+}
