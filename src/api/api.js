13c8598c081c867252bc749fbd79302b03722a03
Ferdi Mehmedov <FredericCo@mail.bg>
2023-02-25 13:05:10 +0200

    Create async function request in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,20 @@
 import { clearUserData, getAccessToken } from "../util.js";
+
+const host = 'http://localhost:3030';
+
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {}
+    };
+	const token = getAccessToken();
+    if(token) {
+        options.headers['X-Authorization'] = token;
+    }
+
+    if(data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+}
