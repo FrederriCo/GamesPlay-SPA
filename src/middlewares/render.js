727da48e2c7f420f2a2bbf9b8afdae5422a2735e
Ferdi Mehmedov <FredericCo@mail.bg>
2023-03-01 10:20:12 +0200

    create render.js and function ctxRender, addReder and create nav template

---
+++ src/middlewares/render.js
@@ -0,0 +1,32 @@
+import { render, html } from '../../node_modules/lit-html/lit-html.js'
+
+const navTemplate = (user) => html`
+            <h1><a class="home" href="/">GamesPlay</a></h1>
+            <nav>
+            <a href="/catalog">All games</a>
+            <!-- Logged-in users -->
+            ${user ? 
+              html`<div id="user">
+                <a href="/create">Create Game</a>
+                <a href="/logout">Logout</a>
+                </div> `
+            : html`<div id="guest">
+                <a href="/login">Login</a>
+                <a href="/register">Register</a>
+        </div>`}
+         
+</nav>`;
+
+const header = document.querySelector('.my-header');
+
+const root = document.getElementById('main-content');
+
+function ctxRender(content) {
+    render(content, root)
+}
+
+export function addRender(ctx, next) {
+    render(navTemplate(ctx.user), header);
+    ctx.render = ctxRender;    
+    next();
+}
