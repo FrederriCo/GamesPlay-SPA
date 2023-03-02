ece387c5d49e4ac7ec65c843d813984e16278e6b
Ferdi Mehmedov <FredericCo@mail.bg>
2023-03-02 11:30:06 +0200

    create register template and function registerView, onSubmit

---
+++ src/views/register.js
@@ -0,0 +1,39 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { createSubmitHandler } from '../util.js'
+import * as userService from '../Services/user.js'
+
+const registerTemplate = (onSubmit) => html`
+<section id="register-page" class="content auth">
+            <form @submit=${onSubmit} id="register">
+                <div class="container">
+                    <div class="brand-logo"></div>
+                    <h1>Register</h1>
+
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="maria@email.com">
+
+                    <label for="pass">Password:</label>
+                    <input type="password" name="password" id="register-password">
+
+                    <label for="con-pass">Confirm Password:</label>
+                    <input type="password" name="confirm-password" id="confirm-password">
+
+                    <input class="btn submit" type="submit" value="Register">
+
+                    <p class="field">
+                        <span>If you already have profile click <a href="#">here</a></span>
+                    </p>
+                </div>
+            </form>
+        </section>`;
+
+export function regiterView(ctx) {
+    ctx.render(registerTemplate(createSubmitHandler(ctx, onSubmit)));
+}
+
+async function onSubmit(ctx, data, event) {
+   
+    await userService.register(data.email, data.password);
+    event.target.reset();  
+    ctx.page.redirect('/');
+}
