67fb765508b81c57a84eb7a6b9a69144bc0a9d85
Ferdi Mehmedov <FredericCo@mail.bg>
2023-03-06 09:42:40 +0200

    Import in app.js catalogView function

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ import { addRender } from './middlewares/render.js';
 import { homeView } from './views/home.js';
 import { regiterView } from './views/register.js';
 import { loginView } from './views/login.js';
+import { catalogView } from './views/catalog.js';
 
 page(addRender);
 
 page('/', homeView);
 page('/register', regiterView);
 page('/login', loginView);
-page('/catalog', () => console.log('catalog'));
+page('/catalog', catalogView);
 page('/create', () => console.log('create'));
 page('/details/:id', () => console.log('details'));
 page('/edit/:id', () => console.log(logout'));
