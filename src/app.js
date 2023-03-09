9ee4d06c99e2502887c8d9ef33aab51069e89c8d
Ferdi Mehmedov <FredericCo@mail.bg>
2023-03-09 16:29:26 +0200

    app.js import addSession

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import page from '../node_modules/page/page.mjs';
 
 import { addRender } from './middlewares/render.js';
+import { addSession } from './middlewares/session.js';
 import { homeView } from './views/home.js';
 import { regiterView } from './views/register.js';
 import { loginView } from './views/login.js';
 import { catalogView } from './views/catalog.js';
 
+page(addSession);
 page(addRender);
 
 page('/', homeView);
